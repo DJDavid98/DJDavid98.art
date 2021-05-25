d87e6a965fed1d15fad7d73a820bff00f820c939
DJDavid98 <djdavid98@protonmail.com>
2021-05-25 03:20:48 +0200

    Don't display exact age above 18 years in StoredAge component

--- src/components/oc/StoredAge.tsx
+++ src/components/oc/StoredAge.tsx
@@ -1,16 +1,20 @@
 import { differenceInYears } from 'date-fns';
-import React, { useEffect, useState, VFC } from 'react';
+import React, { useEffect, useMemo, useState, VFC } from 'react';
 import { TFunction } from 'react-i18next';
 import { useCurrentAge } from 'src/hooks/oc';
 
 export const StoredAge: VFC<{ t: TFunction; className?: string }> = ({ t, className }) => {
   const currentAge = useCurrentAge();
   const [now, setNow] = useState(() => new Date());
+  const displayedAge = useMemo(() => {
+    const calculatedAge = differenceInYears(now, currentAge);
+    return calculatedAge < 18 ? calculatedAge : '18+';
+  }, [currentAge, now]);
 
   useEffect(() => {
     const interval = setInterval(() => setNow(new Date()), 60e3);
     return () => clearInterval(interval);
   }, []);
 
-  return <span className={className}>{t('oc:yourAge', { age: differenceInYears(now, currentAge) })}</span>;
+  return <span className={className}>{t('oc:yourAge', { age: displayedAge })}</span>;
 };
