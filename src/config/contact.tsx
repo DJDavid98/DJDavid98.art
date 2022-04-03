1e3d4ccad30709144134d502162ae09dc5fe949e
DJDavid98 <djdavid98@protonmail.com>
2022-04-03 04:18:22 +0200

    Extract contact details to variables and reorder them

--- src/config/contact.tsx
+++ src/config/contact.tsx
@@ -61,155 +61,179 @@ export type ContactCardProps<T extends ContactCardSettings> = T & {
   wrapInLink?: boolean;
 };
 
+const discord: ContactCardSettings = {
+  id: 'discord',
+  name: 'Discord',
+  url: 'https://discord.djdavid98.art',
+  discordTag: 'DJDavid98#6174',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'discord']} {...props} />,
+  visitText: null,
+};
+const email: ContactCardSettings = {
+  id: 'email',
+  name: (t) => t('about:contact.sendEmail'),
+  url: `mailto:${CONTACT_EMAIL}`,
+  email: CONTACT_EMAIL,
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/protonmail.svg" {...props} />,
+  visitText: null,
+};
+const furbooru: ContactCardSettings = {
+  id: 'furbooru',
+  name: 'Furbooru',
+  url: 'https://furbooru.org/profiles/DJDavid98',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/furbooru.svg" {...props} />,
+};
+const twitter: ContactCardSettings = {
+  id: 'twitter',
+  name: 'Twitter',
+  url: 'https://twitter.com/DJDavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'twitter']} {...props} />,
+};
+const picarto: ContactCardSettings = {
+  id: 'picarto',
+  name: 'Picarto',
+  url: 'https://picarto.tv/DJDavid98',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/picarto.svg" {...props} />,
+  visitText: ['about:contact.visitStream'],
+};
+const youtube: ContactCardSettings = {
+  id: 'youtube',
+  name: 'YouTube',
+  url: 'https://youtube.com/djdavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'youtube']} {...props} />,
+  visitText: ['about:contact.visitChannel'],
+};
+const inkbunny: ContactCardSettings = {
+  id: 'inkbunny',
+  name: 'Inkbunny',
+  url: 'https://inkbunny.net/DJDavid98',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/inkbunny.png" {...props} />,
+};
+const furaffinity: ContactCardSettings = {
+  id: 'furaffinity',
+  name: 'FurAffinity',
+  url: 'https://www.furaffinity.net/user/djdavid98/',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/furaffinity.svg" {...props} />,
+};
+const telegram: ContactCardSettings = {
+  id: 'telegram',
+  name: 'Telegram',
+  url: 'https://t.me/DJDavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'telegram-plane']} {...props} />,
+  visitText: ['about:contact.sendMessage'],
+};
+const steam: ContactCardSettings = {
+  id: 'steam',
+  name: 'Steam',
+  url: 'https://steamcommunity.com/id/djdavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'steam-symbol']} {...props} />,
+};
+const xbox: ContactCardSettings = {
+  id: 'xbox',
+  name: 'Xbox',
+  url: 'http://live.xbox.com/Profile?Gamertag=DJDavidHU',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'xbox']} {...props} />,
+};
+const playstation: ContactCardSettings = {
+  id: 'playstation',
+  name: 'PSN',
+  url: 'https://psnprofiles.com/DJDavidHU',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'playstation']} {...props} />,
+};
+const deviantart: ContactCardSettings = {
+  id: 'deviantart',
+  name: 'DeviantArt',
+  url: 'https://www.deviantart.com/djdavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'deviantart']} {...props} />,
+};
+const pillowfort: ContactCardSettings = {
+  id: 'pillowfort',
+  name: 'Pillowfort',
+  url: 'https://www.pillowfort.social/DJDavid98',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/pillowfort.svg" {...props} />,
+};
+const matrix: ContactCardSettings = {
+  id: 'matrix',
+  name: 'Matrix',
+  url: 'https://matrix.to/#/@disy:djdavid98.art',
+  matrixUsername: '@disy:djdavid98.art',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/matrix.svg" {...props} />,
+  visitText: null,
+};
+const mastodon: ContactCardSettings = {
+  id: 'mastodon',
+  name: 'Mastodon',
+  url: 'https://meow.social/@djdavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'mastodon']} {...props} />,
+};
+const facebook: ContactCardSettings = {
+  id: 'facebook',
+  name: 'Facebook',
+  url: 'https://fb.me/DJDavid1998',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'facebook-square']} {...props} />,
+  visitText: ['about:contact.visitPage'],
+};
+const vk: ContactCardSettings = {
+  id: 'vk',
+  name: 'VK',
+  url: 'https://vk.com/djdavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'vk']} {...props} />,
+  visitText: ['about:contact.visitPage'],
+};
+const newgrounds: ContactCardSettings = {
+  id: 'newgrounds',
+  name: 'Newgrounds',
+  url: 'https://djdavid98.newgrounds.com/',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/newgrounds.png" {...props} />,
+};
+const guilded: ContactCardSettings = {
+  id: 'guilded',
+  name: 'Guilded',
+  url: 'https://guilded.gg/djdavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'guilded']} {...props} />,
+};
+const kongregate: ContactCardSettings = {
+  id: 'kongregate',
+  name: 'Kongregate',
+  url: 'https://www.kongregate.com/accounts/DJDavid98',
+  renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/kongregate.svg" {...props} />,
+};
+const soundcloud: ContactCardSettings = {
+  id: 'soundcloud',
+  name: 'SoundCloud',
+  url: 'https://soundcloud.com/DJDavid98',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'soundcloud']} {...props} />,
+};
+const spotify: ContactCardSettings = {
+  id: 'spotify',
+  name: 'Spotify',
+  url: 'https://open.spotify.com/playlist/4A0PhLrsC5BtKYHIV5kk26?si=5a178f5e614c4548',
+  renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'spotify']} {...props} />,
+  visitText: ['about:contact.playlist'],
+};
+
 export const CONTACT_DETAILS: ContactCardSettings[] = [
-  {
-    id: 'discord',
-    name: 'Discord',
-    url: 'https://discord.djdavid98.art',
-    discordTag: 'DJDavid98#6174',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'discord']} {...props} />,
-    visitText: null,
-  },
-  {
-    id: 'email',
-    name: (t) => t('about:contact.sendEmail'),
-    url: `mailto:${CONTACT_EMAIL}`,
-    email: CONTACT_EMAIL,
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/protonmail.svg" {...props} />,
-    visitText: null,
-  },
-  {
-    id: 'furbooru',
-    name: 'Furbooru',
-    url: 'https://furbooru.org/profiles/DJDavid98',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/furbooru.svg" {...props} />,
-  },
-  {
-    id: 'twitter',
-    name: 'Twitter',
-    url: 'https://twitter.com/DJDavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'twitter']} {...props} />,
-  },
-  {
-    id: 'picarto',
-    name: 'Picarto',
-    url: 'https://picarto.tv/DJDavid98',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/picarto.svg" {...props} />,
-    visitText: ['about:contact.visitStream'],
-  },
-  {
-    id: 'youtube',
-    name: 'YouTube',
-    url: 'https://youtube.com/djdavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'youtube']} {...props} />,
-    visitText: ['about:contact.visitChannel'],
-  },
-  {
-    id: 'inkbunny',
-    name: 'Inkbunny',
-    url: 'https://inkbunny.net/DJDavid98',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/inkbunny.png" {...props} />,
-  },
-  {
-    id: 'furaffinity',
-    name: 'FurAffinity',
-    url: 'https://www.furaffinity.net/user/djdavid98/',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/furaffinity.svg" {...props} />,
-  },
-  {
-    id: 'telegram',
-    name: 'Telegram',
-    url: 'https://t.me/DJDavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'telegram-plane']} {...props} />,
-    visitText: ['about:contact.sendMessage'],
-  },
-  {
-    id: 'steam',
-    name: 'Steam',
-    url: 'https://steamcommunity.com/id/djdavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'steam-symbol']} {...props} />,
-  },
-  {
-    id: 'xbox',
-    name: 'Xbox',
-    url: 'http://live.xbox.com/Profile?Gamertag=DJDavidHU',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'xbox']} {...props} />,
-  },
-  {
-    id: 'playstation',
-    name: 'PSN',
-    url: 'https://psnprofiles.com/DJDavidHU',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'playstation']} {...props} />,
-  },
-  {
-    id: 'deviantart',
-    name: 'DeviantArt',
-    url: 'https://www.deviantart.com/djdavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'deviantart']} {...props} />,
-  },
-  {
-    id: 'pillowfort',
-    name: 'Pillowfort',
-    url: 'https://www.pillowfort.social/DJDavid98',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/pillowfort.svg" {...props} />,
-  },
-  {
-    id: 'matrix',
-    name: 'Matrix',
-    url: 'https://matrix.to/#/@disy:djdavid98.art',
-    matrixUsername: '@disy:djdavid98.art',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/matrix.svg" {...props} />,
-    visitText: null,
-  },
-  {
-    id: 'mastodon',
-    name: 'Mastodon',
-    url: 'https://meow.social/@djdavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'mastodon']} {...props} />,
-  },
-  {
-    id: 'facebook',
-    name: 'Facebook',
-    url: 'https://fb.me/DJDavid1998',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'facebook-square']} {...props} />,
-    visitText: ['about:contact.visitPage'],
-  },
-  {
-    id: 'vk',
-    name: 'VK',
-    url: 'https://vk.com/djdavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'vk']} {...props} />,
-    visitText: ['about:contact.visitPage'],
-  },
-  {
-    id: 'newgrounds',
-    name: 'Newgrounds',
-    url: 'https://djdavid98.newgrounds.com/',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/newgrounds.png" {...props} />,
-  },
-  {
-    id: 'guilded',
-    name: 'Guilded',
-    url: 'https://guilded.gg/djdavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'guilded']} {...props} />,
-  },
-  {
-    id: 'kongregate',
-    name: 'Kongregate',
-    url: 'https://www.kongregate.com/accounts/DJDavid98',
-    renderIcon: (props?: Partial<CustomIconProps>) => <CustomIcon src="/logos/kongregate.svg" {...props} />,
-  },
-  {
-    id: 'soundcloud',
-    name: 'SoundCloud',
-    url: 'https://soundcloud.com/DJDavid98',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'soundcloud']} {...props} />,
-  },
-  {
-    id: 'spotify',
-    name: 'Spotify',
-    url: 'https://open.spotify.com/playlist/4A0PhLrsC5BtKYHIV5kk26?si=5a178f5e614c4548',
-    renderIcon: (props?: Partial<FontAwesomeIconProps>) => <FontAwesomeIcon icon={['fab', 'spotify']} {...props} />,
-    visitText: ['about:contact.playlist'],
-  },
+  discord,
+  telegram,
+  email,
+  matrix,
+  twitter,
+  furbooru,
+  picarto,
+  youtube,
+  inkbunny,
+  steam,
+  xbox,
+  playstation,
+  furaffinity,
+  deviantart,
+  pillowfort,
+  mastodon,
+  facebook,
+  vk,
+  newgrounds,
+  guilded,
+  kongregate,
+  soundcloud,
+  spotify,
 ];
